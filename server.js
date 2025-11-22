fb5791cfffa14476f1421a407aaaf085bd792288
Victor Hugo Lima <victorhugomedlima@gmail.com>
2025-11-22 20:44:05 -0300

    mudança cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
+import cors from "cors";
 
 // Importar rotas
 import usuariosRoutes from './routes/usuarios.js';
